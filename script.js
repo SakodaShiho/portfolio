aa22e7a943d2d0e533717df0e4a74f1937c7c304
SakodaShiho <sawadasakura322@gmail.com>
2021-01-20 01:16:47 +0900

    [add]tab

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// work tab
 
-$('.tab-content>div').hide();
-$('.tab-content>div').first().slideDown();
-  $('.tab-buttons span').click(function(){
-    var thisclass=$(this).attr('class');
-    $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
-    $('.tab-content>div').each(function(){
-      if($(this).hasClass(thisclass)){
-        $(this).fadeIn(800);
-      }
-      else{
-        $(this).hide();
-      }
-    });
-  });
 
-  
+
 // menu スクロールしたら白くなる
 
 window.addEventListener("scroll", function () {
